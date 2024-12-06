f5059cefefab36da1930c44f680857eae850a6f8
Hugo Moison <moison.hugo@gmail.com>
2024-12-06 14:51:38 -0600

    add test and benchmarks minimization

---
+++ benchmarks/minimization/minimization.py
@@ -0,0 +1,31 @@
+""" 
+Tests and benchmark minimization
+"""
+from ase.io import read, write 
+from pykmc.system import System 
+from ase import Atoms 
+import cProfile
+from pstats import Stats
+
+#Parameters : 
+init_config_file = '../../examples/Ni_fcc_499at_monovacancy/Ni_monovacancy.xyz' 
+
+minimization_params = {'min_style' : 'cg',
+                       'minimize'  : '1.0e-6 1.0e-8 100 1000'} 
+potential = {'pair_style' : 'eam/alloy', 
+             'pair_coeff' : '* * ../../examples/Ni_fcc_499at_monovacancy/Ni_v6_2.0_LKBeland2016.eam Ni'}
+nprocs = 2 
+backend = 'local'
+
+#Initialization 
+system = System(init_config_file) 
+traj = [Atoms(positions = system.positions, cell = system.cell, symbols = system.symbols)]
+#Minimization 
+with cProfile.Profile() as profile : 
+    system.minimize('lammps', minimization_params, potential, nprocs = nprocs, backend=backend)
+stats = Stats(profile)
+print("Profiling minimization : ")
+stats.print_stats(0)
+traj.append(Atoms(positions = system.positions, cell = system.cell, symbols = system.symbols))
+#Save to file before and after minimization
+write('traj_minimization.xyz', traj)
---
+++ benchmarks/minimization/zlaunch.sh
@@ -0,0 +1,10 @@
+#for pARTn
+export PYTHONPATH=$PYTHONPATH:/root/programs/artn-plugin/interface
+#for IRA
+export PYTHONPATH=$PYTHONPATH:/root/programs/IterativeRotationsAssignments-master/interface
+
+#to run on root (docker) mpi
+export OMPI_ALLOW_RUN_AS_ROOT=1
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
+
+python3 minimization.py
