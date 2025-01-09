8fa5a66722fc2d1b53f727753ad1e3f88f842a8f
Hugo Moison <moison.hugo@gmail.com>
2025-01-09 12:47:21 -0600

    new example

---
+++ examples/Ni_fcc_2047at_monovacancy/analyse_catalog.py
@@ -0,0 +1,24 @@
+import pandas as pd 
+from ase import Atoms 
+from ase.io import write
+import numpy as np
+
+def write_event_traj(catalog,index) : 
+    """ 
+    write initial positions, saddle positions and final positions to a file
+    """ 
+    traj = [] 
+    col = ['initial_positions', 'saddle_positions', 'final_positions'] 
+    for c in col : 
+        positions = catalog.loc[index].at[c]
+        atoms = Atoms(positions = positions, cell=[17.6,17.6, 17.6], pbc=True)
+        traj.append(atoms) 
+
+    write('event_'+str(index)+'.xyz', traj)
+
+catalog = pd.read_pickle('catalog.pickle') 
+for i in range(len(catalog)):
+    write_event_traj(catalog, i)
+    print('Energy of event {} is {}'.format(i, catalog.loc[i].at['energy_barrier']))
+
+
---
+++ examples/Ni_fcc_2047at_monovacancy/run.py
@@ -0,0 +1,36 @@
+import sys
+from ase.io import read, write
+from pykmc.system import System
+from pykmc.minimization import Minimization 
+from pykmc.atomic_environment import AtomicEnvironment
+from ase.visualize import view
+import cProfile
+import numpy as np
+
+
+#PARAMETERS :
+
+init_config_file = './initial_config.xyz'
+
+minimization = {'min_style'     : 'cg', 
+                'minimize '     : '1.0e-6 1.0e-8 100 1000'}
+
+potential = {'pair_style' : 'eam/alloy', 
+             'pair_coeff' : '* * ./Ni_v6_2.0_LKBeland2016.eam Ni'} 
+
+atomenv = {'rnei' : 3.01, 
+           'rcut' : 7.0}
+
+search_params = {'nsearch' : 10, 
+                 'path_artnso' : '/root/programs/artn-plugin/lib/libartn-lmp.so'}
+                # 'path_artnso' :'/home/hmoison/programs/artn-plugin/lib/libartn-lmp.so' }
+
+kmc_parameters = {'nkmc_steps' : 5}
+#1-Initialize the system : 
+system = System(init_config_file, catalog='catalog.pickle')
+#system = System(init_config_file)
+#KMC 
+system.kmc(kmc_parameters, minimization,atomenv, search_params, potential )
+
+write('kmc_traj.xsf', system.kmc_traj)
+system.catalog.to_pickle('catalog.pickle')
