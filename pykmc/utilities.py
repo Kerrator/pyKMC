b76790798a193ab7a9847fa0d39a8da6b448bd6d
Hugo Moison <moison.hugo@gmail.com>
2025-02-03 17:33:58 -0600

    add initialization lammps function

--- pykmc/utilities.py
+++ pykmc/utilities.py
@@ -19,3 +19,37 @@ def modify_lammps_data_2D(lammps_data_file) :
     #Write new lammps file 
     with open(lammps_data_file, 'w') as file:
         file.writelines(lines) 
+
+
+def initialize_default_lammps(atoms, lmp_instance) : 
+    """ 
+    Initialize the lammps simulation with default settings, metal units, boundary conditions, atomic_style atomic and create box and atoms for 
+    the current system.
+    """
+    from ase.data import atomic_numbers, atomic_masses
+    import numpy as np
+    #System parameters
+    natoms = atoms.get_global_number_of_atoms()
+    cell = atoms.get_cell()
+    xhi, yhi, zhi = cell[0][0], cell[1,1], cell[2,2]
+    type = atoms.get_chemical_symbols()
+    ind = np.linspace(0, atoms.get_global_number_of_atoms()-1, atoms.get_global_number_of_atoms()).astype(int)
+    ind += 1 #Lammps id start at 1
+
+    #map type to int alphabetic order create a dictionary with atom id and mass, eg {'H' : {'ref': 1, 'mass' : 1.00}, 'Ni': {'ref' : 2, 'mass' : 58.69} }
+    map_type = {atom_type: {'ref' :i+1, 'mass' : atomic_masses[atomic_numbers[atom_type]]} for i, atom_type in enumerate(sorted(set(type)))}
+    type = [map_type[element]['ref'] for element in type]
+    x = atoms.get_positions().flatten()
+    
+
+    #lammps command
+    lmp_instance.command('units metal')
+    lmp_instance.command('atom_style atomic')
+    lmp_instance.command('dimension 3') 
+    lmp_instance.command('boundary p p p')
+    lmp_instance.command('atom_modify sort 0 0.0')
+    lmp_instance.command('region box block 0.0 {} 0.0 {} 0.0 {}'.format(xhi, yhi, zhi))
+    lmp_instance.command('create_box 1 box')
+    lmp_instance.create_atoms(natoms, ind,type, x)
+    for key in map_type.keys() : 
+        lmp_instance.command('mass {} {}'.format(map_type[key]['ref'], map_type[key]['mass']))
