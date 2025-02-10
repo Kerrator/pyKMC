f239fff75e90aa762722da8f9511fa99a274b287
Hugo Moison <moison.hugo@gmail.com>
2025-02-10 16:07:33 -0600

    add intertitial example

---
+++ utils/generate_initial_configs/Ni_fcc_interstitial.py
@@ -0,0 +1,45 @@
+import numpy as np
+from ase import Atoms
+from ase.build import bulk, make_supercell
+import random
+from ase.visualize import view
+
+#Generate fcc Ni 
+output = './initial_config.xyz'
+#Ni fcc
+alat = 3.52
+atoms = bulk('Ni', crystalstructure='fcc', a=alat, cubic=True)
+atoms = atoms.repeat((8,8,8))
+print("Number of atoms = ",atoms.get_global_number_of_atoms())
+print("Cell is : ",atoms.get_cell())
+
+cell = atoms.get_cell()
+#Identify interstitial site
+
+#Octahedral and tetrahedral site for fcc 
+octahedral_sites = np.array([[0.5, 0.5, 0.5], [0.5, 0.5, 0.0], [0.5, 0.0, 0.5], [0.0, 0.5, 0.5]])*alat
+tetrahedral_sites = np.array([[0.25, 0.25, 0.25], [0.75, 0.75, 0.75],[0.75, 0.25, 0.25], [0.25, 0.75, 0.25], [0.25, 0.25, 0.75]])*alat
+
+# Repeat interstitial sites on all the box
+all_octahedral_sites = []
+all_tetrahedral_sites = []
+for i in range(0,8):
+    for j in range(0,8):
+        for k in range(0,8):
+            base_shift = np.array([i, j, k])*alat
+            for site in octahedral_sites :
+                all_octahedral_sites.append(site + base_shift)
+            for site in tetrahedral_sites :
+                all_tetrahedral_sites.append(site + base_shift)
+
+
+#Choose a site :
+chosen_site = all_octahedral_sites[1100]
+print("SI site at : ", chosen_site)
+#Add atom in site
+atoms.extend(Atoms('Fe', positions=[chosen_site]))
+
+#save to file
+atoms.write(output)
+
+
