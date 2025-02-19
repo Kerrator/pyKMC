e8f231d14c23ca700aa100eec4c83485d692c714
Hugo Moison <moison.hugo@gmail.com>
2025-02-19 08:05:55 -0600

    SIA+vac example

---
+++ examples/Ni_fcc_4000at_monovacancy+sia/zlaunch.sh
@@ -0,0 +1,12 @@
+#for pARTn
+export PYTHONPATH=$PYTHONPATH:/root/programs/artn-plugin/interface
+#for IRA
+export PYTHONPATH=$PYTHONPATH:/root/programs/IterativeRotationsAssignments-master/interface
+
+#to run on root (docker) mpi
+export OMPI_ALLOW_RUN_AS_ROOT=1
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
+
+
+export PYTHONFAULTHANDLER=1
+python3 run.py
