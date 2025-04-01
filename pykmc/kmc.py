6e6015c3146f10cb38d9950d96a7e4fbfa4283d0
Hugo Moison <moison.hugo@gmail.com>
2025-04-01 13:23:25 -0600

    find new atomic environment

--- pykmc/kmc.py
+++ pykmc/kmc.py
@@ -1,4 +1,6 @@
 from pykmc import System, Engine, Config, NeighborsList, AtomicEnvironment, Catalog
+import random 
+
 
 class KMC() : 
 
@@ -9,28 +11,43 @@ class KMC() :
         self.neighbors_list = None 
         self.atomic_environment = None 
         self.catalog = None
-        self.time = None
-
+        self.visited_environment = set(['crystal'])
             
     def run(self) : 
         
         ###### START ###### 
         self._initialize()
         nkmc_steps = self.config['Control']['nkmc_steps']
-        self.time = 0
+        time = 0
+        nsearch = self.config['EventSearch']['nsearch']
 
         ####### KMC Loop ####
-        for step in range(nkmc_steps) :
-            if step != 0 : 
-                print('update nl et ae')
-            
+        for step in range(1) :
+            #Find new atomic environments that have not been visited
+            new_environment = list(set(self.atomic_environment.atomic_environment_list).difference(self.visited_environment)) 
+            #List of atoms(central) on which we gonna perfom an event search
+            central_atom_research_list = self.central_atoms_research(new_environment, nsearch)
+            #results = self.engine.search_event(self.system, a)
+            #print(results[-1]) 
 
+    def central_atoms_research(self, new_environment, nsearch) : 
+        """ 
+        """
+        central_atom_research_list = []
+        #for each atomic environment hash in new_environment 
+        for env in new_environment :
+            #find all index have that hash
+            tmp1 = [i for i,e in enumerate(self.atomic_environment.atomic_environment_list) if e == env] 
+            #Randomly choose nsearch atoms that have that environment 
+            tmp2 = [random.choice(tmp1) for i in range(nsearch)]
+            central_atom_research_list += tmp2
+        return central_atom_research_list
 
     def _initialize(self) : 
         self.system = System.create_from_file(self.config['Control']['config_file'])
-        engine = Engine(self.config)
+        self.engine = Engine(self.config)
         #minimize 
-        new_positions = engine.minimize(self.system)
+        new_positions = self.engine.minimize(self.system)
         self.system.update_positions(new_positions)
         self.neighbors_list = NeighborsList(self.system, self.config) 
         self.atomic_environment = AtomicEnvironment(self.config, self.neighbors_list.neighbors_list['rnei'], self.neighbors_list.neighbors_list['rcut'])
