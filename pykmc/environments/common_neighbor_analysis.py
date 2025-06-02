20071cfc3ed5f12073f8b6f6d0958db4fab86ff2
Hugo Moison <moison.hugo@gmail.com>
2025-06-02 12:44:42 -0600

    comment clean

--- pykmc/environments/common_neighbor_analysis.py
+++ pykmc/environments/common_neighbor_analysis.py
@@ -1,4 +1,20 @@
 def cna(neighbors_list) : 
+    """
+    Classify atomic environments by neighbor count.
+
+    Determine if each atom's environment is 'crystal' (12, 8, or 6 neighbors)
+    or 'noncrystal'.
+
+    Parameters
+    ----------
+    neighbors_list : list[list[int]]
+        List of first nearest neighbor indices for each atom.
+
+    Returns
+    -------
+    list[str]
+        'crystal' or 'noncrystal' classification for each atom.
+    """
     crystals = [12, 8, 6] 
     hash = [] 
     for neighbors in neighbors_list : 
