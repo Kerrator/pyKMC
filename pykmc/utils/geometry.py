bb7b421f283ab3e47381912ec7c88d25584ff6c9
Hugo Moison <moison.hugo@gmail.com>
2025-06-09 08:42:13 -0600

    move translation to utils geometry

--- pykmc/utils/geometry.py
+++ pykmc/utils/geometry.py
@@ -1,4 +1,12 @@
+import ase.geometry 
+
 def transform_positions(positions, transformation_matrix, translation_matrix, permutation_matrix) : 
     transform_positions = positions @ transformation_matrix.T + translation_matrix 
     return transform_positions[permutation_matrix]
+
+def translate( positions, displacement, cell) : 
+        positions += displacement 
+        positions = ase.geometry.wrap_positions(positions=positions, cell=cell, pbc=True)
+        positions[positions < 0 ] = 0
+        return positions
     
