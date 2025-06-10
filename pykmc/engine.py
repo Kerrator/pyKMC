72ac2a3cad01affa3937b90aece16d6dd6bf30c6
Hugo Moison <moison.hugo@gmail.com>
2025-06-10 12:16:06 -0600

    redirect stdout partn for refinement

--- pykmc/engine.py
+++ pykmc/engine.py
@@ -36,7 +36,15 @@ class Engine() :
     def refine_event(self, system, central_atom_idx:int) : 
         match self.engine.config.eventsearch.style :
             case 'partn' : 
+                original_stdout_fd = os.dup(1)
+                devnull = os.open(os.devnull, os.O_WRONLY)
+                # Redirect stdout (fd 1) to /dev/null, only way to deal with pARTn error write
+                os.dup2(devnull, 1)
                 result = self.engine.pARTn_refine_event(system, central_atom_idx)
+                # Restore original stdout (fd 1)
+                os.dup2(original_stdout_fd, 1)
+                os.close(original_stdout_fd)
+                os.close(devnull)
             case _ : 
                 raise Exception('Event Search style unknown')
         return result
