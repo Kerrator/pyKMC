3b8ce0cac12dea3591700a35985ed9ccb72419a5
Hugo Moison <moison.hugo@gmail.com>
2026-02-03 08:17:05 -0600

    add log number of active event after removing duplicates in active table

--- pykmc/kmc.py
+++ pykmc/kmc.py
@@ -178,6 +178,7 @@ class KMC:
             # == ADD ACTIVE EVENT TO ACTIVE EVENT TABLE ==
             active_table = self.add_active_events(refinement.get_successes_results())
             active_table.remove_duplicates(self.system.cell)  #To be sure
+            self.loggers.info("log", "\t :=> {} active events after removing duplicates.".format(len(active_table.table)))
 
 
             # == Update System ==
