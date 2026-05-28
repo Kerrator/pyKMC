d1318ec392ba07045a862d97489c2d97980ef787
Eugene Sanscartier <eugene.sanscartier@umontreal.ca>
2026-05-28 12:38:39 -0400

    Export new geometry utilities from utils: compute_distances, count_moved_atoms, compute_delr_max

--- pykmc/utils/__init__.py
+++ pykmc/utils/__init__.py
@@ -1,3 +1,10 @@
 from .geometry import *
 
-__all__ = ["transform_positions", "translate", "push_towards", "compute_delr"]
+__all__ = [
+    "transform_positions",
+    "translate",
+    "push_towards",
+    "compute_distances",
+    "count_moved_atoms",
+    "compute_delr_max",
+]
